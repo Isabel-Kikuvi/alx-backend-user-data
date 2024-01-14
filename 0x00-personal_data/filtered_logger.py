cdd57cd7203cb3d6473460f88b6c3d884cea3509
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-01-14 12:42:27 -0800

    regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Module for handling personal data"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Obfuscates fields in a log message using a single regex substitution.
+
+    Args:
+    fields: a list of strings representing all fields to obfuscate
+    redaction: a string representing by what the field will be obfuscated
+    message: a string representing the log line
+    separator: a string representing by which character is separating
+
+    Returns:
+          The obfuscated log message.
+    """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
