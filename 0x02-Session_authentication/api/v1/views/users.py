fc3144b67f703c42699df10308024b5c2493a07d
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-01-28 08:08:27 -0800

    minor edits on documentation

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Module of Users views
+""" 
+Module of Users views
 """
 from api.v1.views import app_views
 from flask import abort, jsonify, request
@@ -9,8 +10,7 @@ from models.user import User
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
     """ GET /api/v1/users
-    Return:
-      - list of all User objects JSON represented
+    Returns list of all User objects JSON represented
     """
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
@@ -19,10 +19,7 @@ def view_all_users() -> str:
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
     """ GET /api/v1/users/:id
-    Path parameter:
-      - User ID
-    Return:
-      - User object JSON represented
+    Returns user object JSON represented
       - 404 if the User ID doesn't exist
     """
     if user_id is None:
@@ -36,10 +33,7 @@ def view_one_user(user_id: str = None) -> str:
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
    """ DELETE /api/v1/users/:id
-    Path parameter:
-      - User ID
-    Return:
-      - empty JSON is the User has been correctly deleted
+    Returns empty JSON is the User has been correctly deleted
       - 404 if the User ID doesn't exist
     """
     if user_id is None:
@@ -59,8 +53,7 @@ def create_user() -> str:
       - password
       - last_name (optional)
       - first_name (optional)
-    Return:
-      - User object JSON represented
+    Returns - User object JSON represented
       - 400 if can't create the new User
     """
     rj = None
@@ -92,8 +85,6 @@ def create_user() -> str:
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id: str = None) -> str:
     """ PUT /api/v1/users/:id
-    Path parameter:
-      - User ID
     JSON body:
       - last_name (optional)
       - first_name (optional)
