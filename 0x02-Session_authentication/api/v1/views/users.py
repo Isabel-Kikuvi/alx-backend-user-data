6a3edc43caba9983db3cf2ff29b1d31526f168b2
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-01-28 08:13:56 -0800

    minor edits

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -32,7 +32,7 @@ def view_one_user(user_id: str = None) -> str:
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
-   """ DELETE /api/v1/users/:id
+    """ DELETE /api/v1/users/:id
     Returns empty JSON is the User has been correctly deleted
       - 404 if the User ID doesn't exist
     """
